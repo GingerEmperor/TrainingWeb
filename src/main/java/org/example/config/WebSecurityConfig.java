918fc1ed023ad4fbe9b1b411118c4d61a6040396
Maxim Colesnic <Maxim.Colesnic@endava.com>
2020-10-06 23:57:21 +0300

    add admin rights

--- src/main/java/org/example/config/WebSecurityConfig.java
+++ src/main/java/org/example/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ package org.example.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
@@ -27,6 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .formLogin()
                     .loginPage("/login")
+//                    .defaultSuccessUrl("/")
                     .permitAll()
                 .and()
                     .logout()
---
+++ src/main/java/org/example/exeptions/UserNotFoundException.java
@@ -0,0 +1,4 @@
+package org.example.exeptions;
+
+public class UserNotFoundException extends RuntimeException {
+}
