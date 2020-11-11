31f99961dd7ec7c6052ef37fa4b9462fc6d84df6
mcolesnic <maxim.colesnic@endava.com>
2020-11-11 09:42:25 +0200

    add muscleFunctions

---
+++ src/main/java/org/example/controllers/TrainingController.java
@@ -0,0 +1,16 @@
+package org.example.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/trainings")
+public class TrainingController {
+
+    @GetMapping()
+    public String showAll(){
+        return "This is Trainings";
+    }
+
+}
