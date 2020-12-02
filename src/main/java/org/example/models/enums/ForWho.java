dc456aad051d3986724859cbe4b507e131d004d9
mcolesnic <maxim.colesnic@endava.com>
2020-12-03 00:15:42 +0200

    Add colorCode in enums

--- src/main/java/org/example/models/enums/ForWho.java
+++ src/main/java/org/example/models/enums/ForWho.java
@@ -5,15 +5,17 @@ import lombok.Getter;
 
 @Getter
 public enum ForWho {
-    BEGINNER("Новичок"),
+    BEGINNER("Новичок","#448d87"),
     // AMATEUR_ATHLETE("Любитель"),
-    ADVANCED("Продвинутый"),
-    PROFESSIONAL("Професионал");
+    ADVANCED("Продвинутый","#4d74a0"),
+    PROFESSIONAL("Професионал","#c95f6c");
     // MONSTER("Монстр");
 
     String world;
+    String colorCode;
 
-    ForWho(final String word) {
+    ForWho(final String word,final String colorCode) {
         this.world = word;
+        this.colorCode=colorCode;
     }
 }
